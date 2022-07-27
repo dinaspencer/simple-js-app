25e20d698bce2f52649b31f843c88b334ed6c592
Dina Spencer <dkay22@gmail.com>
2022-07-27 10:41:17 +0200

    Added pokemon list of objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,1 @@
-alert('Hello world');
-
-let favoriteFood = "guacamole";
-document.write(favoriteFood);
+let pokemonList = [{name: 'Venusaur', height: 2, type: ['grass', 'poison']}, {name: 'Butterfree', height: 1, type: ['bug', 'flying']}, {name: 'Sandslash', height: 1.1, type: 'ground'}, {name: 'Psyduck', height: 0.8, type: 'water'}, {name: 'Slowpoke', height: 1.2, type: ['psychic', 'water']}, {name: 'Quilava', height: 0.9, type: 'fire'}, {name: 'Breloom', height: 1.2, type: ['grass', 'fighting']}, {name: 'Misdreavus', height: 0.7, type: 'ghost'}];
