cfe473ea944f34d2333e85d9dc8e6800a28e446b
Dina Spencer <dkay22@gmail.com>
2022-07-28 10:29:43 +0200

    Added loop and conditional to create a list of pokemons and their heights, added styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-let pokemonList = [{name: 'Venusaur', height: 2, type: ['grass', 'poison']}, {name: 'Butterfree', height: 1, type: ['bug', 'flying']}, {name: 'Sandslash', height: 1.1, type: 'ground'}, {name: 'Psyduck', height: 0.8, type: 'water'}, {name: 'Slowpoke', height: 1.2, type: ['psychic', 'water']}, {name: 'Quilava', height: 0.9, type: 'fire'}, {name: 'Breloom', height: 1.2, type: ['grass', 'fighting']}, {name: 'Misdreavus', height: 0.7, type: 'ghost'}];
+let pokemonList = [
+  {name: 'Venusaur', height: 2, type: ['grass', 'poison']},
+  {name: 'Butterfree', height: 1, type: ['bug', 'flying']},
+  {name: 'Sandslash', height: 1.1, type: 'ground'},
+  {name: 'Psyduck', height: 0.8, type: 'water'},
+  {name: 'Slowpoke', height: 1.2, type: ['psychic', 'water']},
+  {name: 'Quilava', height: 0.9, type: 'fire'},
+  {name: 'Breloom', height: 1.2, type: ['grass', 'fighting']},
+  {name: 'Misdreavus', height: 0.7, type: 'ghost'}
+];
+//this makes a list of pokemons
+for (let i=0; i<pokemonList.length; i++) {
+
+  if (pokemonList[i].height >= 2) {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +")" + " - Wow, a big pokemon!");
+    document.write("<br>");
+  }else {
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +")");
+  document.write("<br>");
+}
+}
